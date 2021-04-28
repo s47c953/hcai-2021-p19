aaeb2f0ac9e5cd962a559e8e4d942220a7b2a272
Udo <udo.grossschedl@gmail.com>
2021-04-28 15:18:21 +0200

    further improved aggregation functions

--- AggregationFunction.py
+++ AggregationFunction.py
@@ -1,8 +1,7 @@
-
 # abstract base
 class AggregationFunction:
     @staticmethod
-    def perform(x: float, y: float, k: float = 1) -> float:
+    def perform(x: float, y: float, l: float = 1, r: float = 1) -> float:
         raise NotImplementedError
 
     @staticmethod
@@ -19,43 +18,42 @@ class AggregationFunction:
         raise NotImplementedError
 
     @staticmethod
-    def findMarkerFromFunction(k: float, x_start: float, y_start: float, start_value: float, inc: float,  func, inc_x = True):
+    def findMarkerFromFunction(l: float, r: float, x_start: float, y_start: float, start_value: float, inc: float, func, inc_x=True):
         # random init
         x = x_start
         y = y_start
-        res = func(x, y, k)
+        res = func(x, y, k, r)
         while res == start_value:
             if inc_x:
                 x += inc
             else:
                 y += inc
-            res = func(x, y, k)
+            res = func(x, y, k, r)
         return x, y
-
 
 
 class LukasiewiczAggregationFunction(AggregationFunction):
     @staticmethod
-    def perform(x: float, y: float, k: float = 1) -> float:
+    def perform(x: float, y: float, l: float = 1, r: float = 1) -> float:
         if x < 0.5 and y < 0.5:
-            return LukasiewiczAggregationFunction._noFunction(x, y, k)
+            return LukasiewiczAggregationFunction._noFunction(x, y, l, r)
         elif x < 0.5 or y < 0.5:
-            return LukasiewiczAggregationFunction._maybeFunction(x, y, k)
+            return LukasiewiczAggregationFunction._maybeFunction(x, y, l, r)
         elif x >= 0.5 and y >= 0.5:
-            return LukasiewiczAggregationFunction._yesFunction(x, y, k)
+            return LukasiewiczAggregationFunction._yesFunction(x, y, l, r)
 
     @staticmethod
-    def _yesFunction(x: float, y: float, k: float) -> float:
-        return min(1.0, (x**k+y**k-0.5**k))**(1/k)
+    def _yesFunction(x: float, y: float, l: float, r: float = 1) -> float:
+        return min(1.0, (x ** (l*r) + y ** (l*r) - 0.5 ** l ** (1 / l)
 
     @staticmethod
-    def _noFunction(x: float, y: float, k: float) -> float:
-        return max(0.0, (x**k+y**k-0.5**k))**(1/k)
+    def _noFunction(x: float, y: float, l: float, r: float = 1) -> float:
+        return max(0.0, (x ** (l*r) + y ** (l*r) - 0.5 ** l ** (1 / (l*r))
 
     @staticmethod
-    def _maybeFunction(x: float, y: float, k: float) -> float:
+    def _maybeFunction(x: float, y: float, l: float, r: float = 1) -> float:
         # TODO: check if real median or just middle value of 0, 1, x+y-1/2 is needed
-        val = (x**k+y**k-0.5**k)**(1/k)
+        val = (x ** (l*r) + y ** (l*r) - 0.5 ** l ** (1 / (l*r))
         if val < 0:
             return 0.0
         elif val > 1:
@@ -64,11 +62,19 @@ class LukasiewiczAggregationFunction(AggregationFunction):
             return val
 
     @staticmethod
-    def getMarker(k: float) -> {}:
-        upper_marker_x, upper_marker_y = AggregationFunction.findMarkerFromFunction(k, 1, 1, 1, -0.01, LukasiewiczAggregationFunction._yesFunction, True)
-        lower_marker_x, lower_marker_y = AggregationFunction.findMarkerFromFunction(k, 0, 0, 0, 0.01, LukasiewiczAggregationFunction._noFunction, True)
-        right_marker_x, right_marker_y = AggregationFunction.findMarkerFromFunction(k, 1, 1, 1, -0.01, LukasiewiczAggregationFunction._yesFunction, False)
-        left_marker_x, left_marker_y = AggregationFunction.findMarkerFromFunction(k, 0, 0, 0, 0.01, LukasiewiczAggregationFunction._noFunction, False)
+    def getMarker(l: float) -> {}:
+        upper_marker_x, upper_marker_y = AggregationFunction.findMarkerFromFunction(l, 1, 1, 1, -0.01,
+                                                                                    LukasiewiczAggregationFunction._yesFunction,
+                                                                                    True)
+        lower_marker_x, lower_marker_y = AggregationFunction.findMarkerFromFunction(l, 0, 0, 0, 0.01,
+                                                                                    LukasiewiczAggregationFunction._noFunction,
+                                                                                    True)
+        right_marker_x, right_marker_y = AggregationFunction.findMarkerFromFunction(l, 1, 1, 1, -0.01,
+                                                                                    LukasiewiczAggregationFunction._yesFunction,
+                                                                                    False)
+        left_marker_x, left_marker_y = AggregationFunction.findMarkerFromFunction(l, 0, 0, 0, 0.01,
+                                                                                  LukasiewiczAggregationFunction._noFunction,
+                                                                                  False)
 
         return {'ux': upper_marker_x, 'uy': upper_marker_y, 'lox': lower_marker_x, 'loy': lower_marker_y,
                 'rx': right_marker_x, 'ry': right_marker_y, 'lx': left_marker_x, 'ly': left_marker_y}
@@ -76,47 +82,47 @@ class LukasiewiczAggregationFunction(AggregationFunction):
 
 class MinMaxAggregationFunction(AggregationFunction):
     @staticmethod
-    def perform(x: float, y: float, k: float = 1) -> float:
+    def perform(x: float, y: float, l: float = 1, r: float = 1) -> float:
         if x < 0.5 and y < 0.5:
-            return MinMaxAggregationFunction._noFunction(x, y)
+            return MinMaxAggregationFunction._noFunction(x, y, l, r)
         elif x < 0.5 or y < 0.5:
-            return MinMaxAggregationFunction._maybeFunction(x, y)
+            return MinMaxAggregationFunction._maybeFunction(x, y, l, r)
         elif x >= 0.5 and y >= 0.5:
-            return MinMaxAggregationFunction._yesFunction(x, y)
+            return MinMaxAggregationFunction._yesFunction(x, y, l, r)
 
     @staticmethod
-    def _yesFunction(x: float, y: float, k: float) -> float:
+    def _yesFunction(x: float, y: float, l: float, r: float = 1) -> float:
         return max(x, y)
 
     @staticmethod
-    def _noFunction(x: float, y: float, k: float) -> float:
+    def _noFunction(x: float, y: float, l: float, r: float = 1) -> float:
         return min(x, y)
 
     @staticmethod
-    def _maybeFunction(x: float, y: float, k: float) -> float:
+    def _maybeFunction(x: float, y: float, l: float, r: float = 1) -> float:
         # TODO: check if real median or just middle value of 0, 1, x+y-1/2 is needed
-        return x**k+y**k-0.5**k
+        return x ** (l*r) + y ** (l*r) - 0.5 ** l
 
 
 class TnormTconormAggregationFunction(AggregationFunction):
     @staticmethod
-    def perform(x: float, y: float, k: float = 1) -> float:
+    def perform(x: float, y: float, l: float = 1, r: float = 1) -> float:
         if x < 0.5 and y < 0.5:
-            return TnormTconormAggregationFunction._noFunction(x, y)
+            return TnormTconormAggregationFunction._noFunction(x, y, l, r)
         elif x < 0.5 or y < 0.5:
-            return TnormTconormAggregationFunction._maybeFunction(x, y)
+            return TnormTconormAggregationFunction._maybeFunction(x, y, l, r)
         elif x >= 0.5 and y >= 0.5:
-            return TnormTconormAggregationFunction._yesFunction(x, y)
+            return TnormTconormAggregationFunction._yesFunction(x, y, l, r)
 
     @staticmethod
-    def _yesFunction(x: float, y: float, k: float) -> float:
-        return -1+2*x+2*y-2*x*y
+    def _yesFunction(x: float, y: float, l: float, r: float = 1) -> float:
+        return -1 + 2 * x + 2 * y - 2 * x * y
 
     @staticmethod
-    def _noFunction(x: float, y: float, k: float) -> float:
+    def _noFunction(x: float, y: float, l: float, r: float = 1) -> float:
         return TnormTconormAggregationFunction._maybeFunction(x, y)
 
     @staticmethod
-    def _maybeFunction(x: float, y: float, k: float) -> float:
+    def _maybeFunction(x: float, y: float, l: float, r: float = 1) -> float:
         # TODO: check if real median or just middle value of 0, 1, x+y-1/2 is needed
-        return 2*x*y
+        return 2 * x * y
